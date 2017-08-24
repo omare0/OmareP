9820c40c589258ee381b76693b7eddc632d31a23
Omar Paredes <omare.paredes@gmail.com>
2017-08-23 21:37:33 -0500

    Pantalla Pendientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MainComponent } from './components/main/main.component';
+import { PendingsComponent } from './components/pendings/pendings.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		NavbarComponent,
 		SidebarComponent,
-		MainComponent
+		MainComponent,
+		PendingsComponent
 		],
 	imports: [
 		BrowserModule,
---
+++ src/app/components/pendings/pendings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-pendings',
+	templateUrl: './pendings.component.html'
+})
+
+export class PendingsComponent {
+
+	constructor() { }
+
+}
